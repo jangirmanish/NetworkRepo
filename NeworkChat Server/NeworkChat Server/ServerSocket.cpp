6fb70c158168fce6e1afc2102dc8c63d75b57fbd
jangirmanish <jangirmanish@live.in>
2016-04-02 15:39:55 +0530

    Simple client server message recieved

---
+++ NeworkChat Server/NeworkChat Server/ServerSocket.cpp
@@ -0,0 +1,9 @@
+//
+//  ServerSocket.cpp
+//  NeworkChat Server
+//
+//  Created by Manish Jangir on 02/04/16.
+//  Copyright © 2016 Manish Jangir. All rights reserved.
+//
+
+#include "ServerSocket.hpp"
---
+++ NeworkChat Server/NeworkChat Server/ServerSocket.hpp
@@ -0,0 +1,14 @@
+//
+//  ServerSocket.hpp
+//  NeworkChat Server
+//
+//  Created by Manish Jangir on 02/04/16.
+//  Copyright © 2016 Manish Jangir. All rights reserved.
+//
+
+#ifndef ServerSocket_hpp
+#define ServerSocket_hpp
+
+#include <stdio.h>
+
+#endif /* ServerSocket_hpp */
